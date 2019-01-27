ea26b03bfcd5e2bd2ac5d343c5c85319959a3c69
duclt <duclt@ghtk.vn>
2019-01-27 15:30:10 +0700

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -17,9 +17,27 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected');
 
+  // socket.emit from Admin text Welcom to the chat app
+  // socket.broadcast.emit from Admin text New user joined
+
+  socket.emit('greeting', {
+    text: 'Welcome to the chat app'
+  });
+
+  socket.broadcast.emit('userjoin', {
+    text: 'New user joined'
+  });
+
+
   socket.on('createMessage', (message) => {
     console.log('createEmail', message);
-    io.emit('newMessage', {
+    // io.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createAt: new Date().getTime()
+    // })
+
+    socket.broadcast.emit('newMessage', {
       from: message.from,
       text: message.text,
       createAt: new Date().getTime()
