472d905ca72083dae55fa2bf657b3d94f74d2ff2
duclt <duclt@ghtk.vn>
2019-02-05 14:22:52 +0700

    scroll to bottom if user is close to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,23 @@
 var socket = io();
+
+function scrollToBottom() {
+  // selectors
+  var messages = jQuery('#messages');
+  var newMessage = messages.children('li:last-child');
+
+  //heights
+  var clientHeight = messages.prop('clientHeight');
+  var scrollTop = messages.prop('scrollTop');
+  var scrollHeight = messages.prop('scrollHeight');
+  var newMessageHeight = newMessage.innerHeight();
+  var lastMessageHeight = newMessage.prev().innerHeight();
+
+  if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+    messages.scrollTop(scrollHeight)
+  }
+}
+
+
 socket.on('connect', () => {
   console.log('Connected to server');
 });
@@ -17,7 +36,7 @@ socket.on('newMessage', (message) => {
   });
 
   jQuery('#messages').append(html);
-
+  scrollToBottom();
 
   // //console.log('newMessage', message);
   // var li = jQuery('<li></li>');
@@ -36,6 +55,7 @@ socket.on('newLocationMessage', function (message) {
   });
 
   jQuery('#messages').append(html);
+  scrollToBottom();
   //
   // var li = jQuery('<li></li>');
   // var a = jQuery('<a target="_blank">My current location</a>');
